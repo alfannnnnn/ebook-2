a749ca40d94c0faad18e7995a7f7c453a919f774
alfan <139678785+alfannnnnn@users.noreply.github.com>
2026-01-16 23:48:39 +0800

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,31 +6,30 @@ const Hero = () => {
     <div className="text-center bg-[#0d4875] p-6">
       <div className="flex flex-col gap-4">
         <h1 className="text-4xl font-bold text-[#ffff00]">
-          🕌 Mau Anak Cinta Islam Sejak Dini?
+          🕌 Ubah Waktu Bermain Si Kecil Menjadi Investasi Pahala Jariyah untuk Ayah Bunda
         </h1>
         <p className="font-bold text-white text-lg">
-          Cukup Gunakan Worksheet Ini, dan Lihat Anak Belajar dengan Gembira! 😍
+          Bukan sekadar bermain, tapi aktivitas yang menanam nilai islami sejak dini dan terus mengalirkan pahala.😍
         </p>
         <div className="my-4 border-4 border-blue-500 rounded-md shadow-md">
           <img src={imageList[0].url} alt={imageList[0].alt} />
         </div>
         <p className="text-lg text-white">
-          Lebih dari{" "}
+          Kini hadir! {" "}
           <span className="font-bold">
-            1200+ lembar aktivitas anak tema Islami
+            1200+ Halaman Worksheet Islami
           </span>{" "}
-          siap pakai Didesain khusus untuk anak usia 3 tahun ke atas — belajar
-          Tauhid, Akhlak, Ibadah, Sholat, Doa Harian, dll
+          Penuh Warna yang Didesain Khusus Agar Si Kecil Belajar Sambil Bermain Tanpa Merasa Bosan
         </p>
         <div className="p-4 my-4 rounded-md shadow-md text-start bg-white">
           <h3 className="text-2xl font-bold mb-4">
-            📌 Masalah Umum Para Orang Tua:
+            📌 Ayah Bunda, apakah ini yang sedang dirasakan?:
           </h3>
-          <p>❌ Anak lebih tertarik main HP daripada belajar</p>
-          <p>❌ Susah cari bahan belajar agama yang sesuai usia anak</p>
-          <p>❌ Gak ada waktu nyusun aktivitas edukatif tiap hari</p>
+          <p>❌ Si kecil lebih cepat menghafal lagu tiktok daripada doa-doa harian</p>
+          <p>❌ Ingin aktivitas yang mendidik, tapi tetap seru dan nggak bikin anak bosan</p>
+          <p>❌ Waktu berkualitas bersama anak sering tergeser oleh gadget</p>
           <p>❌ Buku belajar mahal & cepat habis isinya</p>
-          <p>❌ Ingin anak cinta Islam, tapi bingung harus mulai dari mana</p>
+          <p>❌ Ingin menanamkan nilai-nilai Islam, tapi masih bingung cara yang menyenangkan</p>
         </div>
         <p className="text-lg text-[#ffff00] font-bold">
           Kalau Bunda pernah ngalamin ini, tenang... <br /> Sekarang ada solusi
@@ -42,3 +41,4 @@ const Hero = () => {
 };
 
 export default Hero;
+
