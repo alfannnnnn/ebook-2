edc61919a58f77aee19a413abf61bb9feded67af
alfan <139678785+alfannnnnn@users.noreply.github.com>
2026-01-18 07:19:04 +0800

    Add files via upload

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,31 +5,44 @@ const Hero = () => {
   return (
     <div className="text-center bg-[#0d4875] p-6">
       <div className="flex flex-col gap-4">
-        <h1 className="text-4xl font-bold text-[#ffff00]">
-          🕌 Ubah Waktu Bermain Si Kecil Menjadi Investasi Pahala Jariyah untuk Ayah Bunda
+        <h1 className="text-2xl font-bold text-[#ffff00]">
+          🕌 Ubah Waktu Bermain Si Kecil Menjadi{" "}
+          <span className="text-white">Investasi Pahala Jariyah</span> untuk
+          Ayah Bunda
         </h1>
-        <p className="font-bold text-white text-lg">
-          Bukan sekadar bermain, tapi aktivitas yang menanam nilai islami sejak dini dan terus mengalirkan pahala.😍
+        <p className=" text-white text-lg">
+          Bukan sekadar bermain, tapi aktivitas yang menanam nilai islami sejak
+          dini dan terus mengalirkan pahala.😍
         </p>
         <div className="my-4 border-4 border-blue-500 rounded-md shadow-md">
           <img src={imageList[0].url} alt={imageList[0].alt} />
         </div>
         <p className="text-lg text-white">
-          Kini hadir! {" "}
-          <span className="font-bold">
+          Kini hadir!{" "}
+          <span className="font-bold text-[#ffff00]">
             1200+ Halaman Worksheet Islami
           </span>{" "}
-          Penuh Warna yang Didesain Khusus Agar Si Kecil Belajar Sambil Bermain Tanpa Merasa Bosan
+          Penuh Warna yang Didesain Khusus Agar Si Kecil Belajar Sambil Bermain
+          Tanpa Merasa Bosan
         </p>
         <div className="p-4 my-4 rounded-md shadow-md text-start bg-white">
-          <h3 className="text-2xl font-bold mb-4">
+          <h3 className="text-xl font-bold mb-4">
             📌 Ayah Bunda, apakah ini yang sedang dirasakan?:
           </h3>
-          <p>❌ Si kecil lebih cepat menghafal lagu tiktok daripada doa-doa harian</p>
-          <p>❌ Ingin aktivitas yang mendidik, tapi tetap seru dan nggak bikin anak bosan</p>
+          <p>
+            ❌ Si kecil lebih cepat menghafal lagu tiktok daripada doa-doa
+            harian
+          </p>
+          <p>
+            ❌ Ingin aktivitas yang mendidik, tapi tetap seru dan nggak bikin
+            anak bosan
+          </p>
           <p>❌ Waktu berkualitas bersama anak sering tergeser oleh gadget</p>
           <p>❌ Buku belajar mahal & cepat habis isinya</p>
-          <p>❌ Ingin menanamkan nilai-nilai Islam, tapi masih bingung cara yang menyenangkan</p>
+          <p>
+            ❌ Ingin menanamkan nilai-nilai Islam, tapi masih bingung cara yang
+            menyenangkan
+          </p>
         </div>
         <p className="text-lg text-[#ffff00] font-bold">
           Kalau Bunda pernah ngalamin ini, tenang... <br /> Sekarang ada solusi
@@ -41,4 +54,3 @@ const Hero = () => {
 };
 
 export default Hero;
-
--- src/components/Topik.jsx
+++ src/components/Topik.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { topikImage } from "../utils.js";
+// import { topikImage } from "../utils.js";
 
 const Topik = () => {
   return (
@@ -11,7 +11,35 @@ const Topik = () => {
         📖 <span className="font-bold">Total 24 topik Islami,</span> <br />{" "}
         yaitu:
       </p>
-      <img src={topikImage[0].url} alt={topikImage[0].alt} />
+      {/* <img src={topikImage[0].url} alt={topikImage[0].alt} /> */}
+      <div>
+        <p>
+          ✅️ Worksheet puasa (10 lembar) <br /> ✅️ Rukun iman & islam (7
+          lembar) <br /> ✅️ Sholat & Wudhu (21 Lembar) <br /> ✅️ Sifat-sifat
+          Allah (20 Lembar) <br />
+          ✅️ Hadits-hadits pendek (25 Lembar) <br /> ✅️ Haji & Umroh (3
+          lembar) <br />
+          ✅️ Hari Besar Islam (23 lembar)
+          <br /> ✅️ Hari Kiamat (4 lembar)
+          <br /> ✅️ Angka bahasa arab (83 lembar)
+          <br /> ✅️ Asmaul Husna (77 lembar)
+          <br /> ✅️ Bulan Hijriyah (18 lembar)
+          <br /> ✅️ Doa-doa pendek (22 lembar)
+          <br /> ✅️ Huruf Hijaiyah (235 lembar) <br />
+          ✅️ Kalimat Thoyyibah (15 lembar)
+          <br /> ✅️ Makanan Halal (19 lembar)
+          <br /> ✅️ Mengenal Harakat (3 lembar)
+          <br /> ✅️ Nabi & Rasul (54 lembar)
+          <br />
+          ✅️ Najis dalam islam (4 lembar)
+          <br /> ✅️ Nama Malaikat (5 lembar)
+          <br /> ✅️ Perbuatan Terpuji (52 lembar)
+          <br /> ✅️ Surat-surat pendek (34 lembar) <br />
+          ✅️ Surga neraka (3 lembar) <br />
+          ✅️ Mewarnai (100 lembar)
+          <br /> ✅️ lain-lain (176 lembar)
+        </p>
+      </div>
       <div className="px-8">
         <p className="font-bold text-center my-8 text-lg">
           Semua disusun rapi dalam file PDF, gampang diakses & dicetak kapan
